6fc44fcfe886759244c98e7e92343b29923e71f4
philtreez <philipp.treezy@gmail.com>
2025-02-02 08:21:24 +0100

    m

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ const phonemeDictionary = {
 class TrashyChatbot {
     constructor() {
         this.memory = [];
-        this.name = "BitBuddy"; // Assistant's name
+        this.name = "RoboPhil"; // Assistant's name
         this.introduction = [
             `Hi, I’m ${this.name}, your assistant. Philipp is busy with *very important* things, so I’m in charge now!`,
             `Hello, I’m ${this.name}. Philipp told me to handle things while he works on *groundbreaking* projects. So... hi!`,
